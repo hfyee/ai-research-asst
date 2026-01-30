b6f9f33bfd8f5693ac875f938bca796459057a58
hfyee <hoong@EricY-TP-P14S.localdomain>
2026-01-31 03:34:11 +0800

    edit comments

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ with st.sidebar:
     image_url = st.text_input("Product image for color variation:", value="input_files/Tern-Verge-D9-black.jpg")
     #uploaded_image = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
     #image_url = uploaded_image.name if uploaded_image else ""
-    '''
+    """
     # As uploaded file is temporarily held in server RAM, we need to save it to a temp file for use with functions that require a file path
     if uploaded_image:
         # Create a temporary directory and file
@@ -79,7 +79,7 @@ with st.sidebar:
             if image_url:
                 st.sidebar.image(image_url, caption='Product original image', width=200)
             new_color = st.text_input("New color for product variant:", placeholder="e.g., white, blue, gold, red, green")
-    '''
+    """
     st.divider()
     st.info("Version v0.2.0")
 
